27a85aadd334d39fe0e4c4c491bdc341d0980163
Abdur Rahman <abdurrahman.offical.63@gmail.com>
2023-01-06 23:11:52 +0600

    added wishlist page

--- components/wishlist/WishlistTableRow.tsx
+++ components/wishlist/WishlistTableRow.tsx
@@ -27,46 +27,49 @@ const WishlistTableRow = ({ product }: Product) => {
 
   return (
     <>
-      <Tr className={"sm:border-b border-[rgba(100,48,1,0.3)] "}>
-        <Td className="py-4 pl-4 w-[550px] text-[15px] leading-6 font-[500] ">
-          <div className="flex items-center gap-3 lg:gap-6">
-            <CgTrash
-              onClick={() => handleWishlistDelete(product?._id)}
-              className="hidden text-[#031424] cursor-pointer sm:block text-[20px]"
-            />
-
-            <div className="flex items-center gap-8">
+      <Tr className={"sm:border-b border-x border-[#D9D9D9]"}>
+        <Td className="border-r py-[14px] border-[#D9D9D9] text-center">
+          <input
+            type="checkbox"
+            className="checkbox checkbox-primary border border-[#7C7C7C] rounded-none w-[15px] h-[15px]"
+          />
+        </Td>
+        <Td className="border-r py-[14px]">
+          <CgTrash
+            onClick={() => handleWishlistDelete(product?._id)}
+            className="text-[#031424] sm:mx-auto cursor-pointer text-[24px]"
+          />
+        </Td>
+        <Td className="sm:py-[14px] py-[14px] pl-4 xl:w-[500px] lg:w-[300px] sm:w-[220px]  2xl:text-[17px] text-[13px] lg:text-[15px] leading-6 font-[500] ">
+          <div className="flex items-center gap-3 lg:gap-5 xl:gap-[46px]">
+            <figure className="p-[6px] rounded-[8px] bg-[#F8F8F8]">
               <Image
-                height={100}
-                width={103}
-                className={"xl:h-[100px] xl:w-[103px] h-[50px] w-[48px]"}
+                height={80}
+                width={80}
+                className={"xl:h-[80px] xl:w-[80px] h-[50px] w-[48px]"}
                 src={`${product?.img[0]}`}
                 alt="product-img"
               />
-              <h3 className="text-info">{product?.name}</h3>
-            </div>
+            </figure>
+            <h3 className="text-info ">{product?.name}</h3>
           </div>
         </Td>
-        <Td className="text-[15px]sm:pl-3 sm:py-4 pb-4 leading-[25px] font-[500] ">
+        <Td className="2xl:text-[17px] py-[14px] text-[13px] lg:text-[15px] text-center sm:py-[14px] pb-4 leading-[25px] text-[#424242] font-[500] ">
           ${product?.price.toFixed(2)}
         </Td>
-        <Td className="text-[17px] font-[500] leading-[25px] text-[#424242]">
+        <Td className="2xl:text-[17px] py-[14px] text-[13px] lg:text-[15px]  text-center font-[500] leading-[25px] text-[#424242]">
           {format(new Date(product.updatedAt), "MMM dd, yyyy")}
         </Td>
-        <Td className="text-[17px] font-[500] leading-[25px] text-[#424242]">{product?.status}</Td>
-        <Td className="text-[15px] sm:py-4 pb-4 text-center leading-[25px] font-[500] ">
+        <Td className="2xl:text-[17px] py-[14px] text-[13px] lg:text-[15px]  text-center lg:w-[150px] sm:w-[120px] font-[500] leading-[25px] text-[#424242]">
+          {product?.status}
+        </Td>
+        <Td className="2xl:text-[17px] max-w-[160px] text-[13px] lg:text-[15px] py-[14px] pb-4 text-center leading-[25px] font-[500] ">
           <button
             onClick={() => handleAddToCart(product?._id)}
-            className="text-[20px] text-[#000000] leading-[30px] bg-primary w-[154px] h-[50px] rounded-[8px]"
+            className="xl:text-[18px] md:text-[15px] text-sm text-[#000000] leading-[30px] bg-primary w-[100px] h-[40px] md:w-[110px] md:h-[42px] xl:w-[154px] xl:h-[50px] rounded-[8px]"
           >
             Add to cart
           </button>
-        </Td>
-        <Td
-          onClick={() => handleWishlistDelete(product?._id)}
-          className="text-[15px]  cursor-pointer sm:py-4 pb-4 sm:hidden text-center leading-[25px] font-[500] text-[#031424]"
-        >
-          <CgTrash onClick={() => handleWishlistDelete(product?._id)} />
         </Td>
       </Tr>
     </>
